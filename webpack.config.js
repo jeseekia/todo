7e50a7a4bece512f3d936cac0abec700b01d4ce0
Jeseekia Vaughn <jeseekia@gmail.com>
2016-02-25 16:12:27 -0500

    Configuring the development server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -12,7 +13,17 @@ module.exports = {
     path: PATHS.build,
     filename: 'bundle.js'
   },
+  devServer: {
+    hot: true,
+    inline: true,
+    progress: true,
+    historyApiFallback: true,
+    stats: 'errors-only',
+    port: process.env.PORT,
+    host: process.env.HOST
+  },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlwebpackPlugin({
       title: 'Too Dew'
     })
